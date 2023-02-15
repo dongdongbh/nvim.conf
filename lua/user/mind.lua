7a980fd44020d1522fe5e50577a6c4f1a8963e10
dongdongbh <18310682633@163.com>
2023-02-15 17:47:02 +0800

    setup mind

--- lua/user/mind.lua
+++ lua/user/mind.lua
@@ -1,1 +1,9 @@
-require'mind'.setup()
+require'mind'.setup({
+  persistence = {
+    data_dir = "~/Documents/notes",
+  },
+  edit = {
+    data_extension=".norg",
+    data_header = "* %s",
+  }
+})

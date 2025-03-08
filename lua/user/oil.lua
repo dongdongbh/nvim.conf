a77c180a651972a2627f39c99ce434456251e668
dongdongbh <18310682633@163.com>
2025-03-08 12:56:26 -0500

    nil

--- lua/user/oil.lua
+++ lua/user/oil.lua
@@ -1,15 +1,16 @@
 local M = {
-  "stevearc/oil.nvim",
-  lazy = false, -- not lazy loaded because it needs to work in all situations
-  opts = {},
-  dependencies = {
-    { "echasnovski/mini.icons", opts = {} },
-    -- You can also use: { "nvim-tree/nvim-web-devicons" },
-  },
+	"stevearc/oil.nvim",
+	lazy = false, -- not lazy loaded because it needs to work in all situations
+	opts = {},
+	dependencies = {
+		{ "echasnovski/mini.icons", opts = {} },
+		-- You can also use: { "nvim-tree/nvim-web-devicons" },
+	},
 }
 
 function M.config()
-  require("oil").setup(M.opts)
+	require("oil").setup(M.opts)
+	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory (Oil)" })
 end
 
 return M
--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,54 +1,48 @@
 local M = {
-  "akinsho/toggleterm.nvim",
-  event = "VeryLazy",
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
 }
 
 function M.config()
-  local status_ok, toggleterm = pcall(require, "toggleterm")
-  if not status_ok then
-    return
-  end
+	local status_ok, toggleterm = pcall(require, "toggleterm")
+	if not status_ok then
+		return
+	end
 
-  toggleterm.setup {
-    size = function(term)
-      if term.direction == "horizontal" then
-        return 10                              -- height for horizontal split terminals
-      elseif term.direction == "vertical" then
-        return math.floor(vim.o.columns * 0.4) -- width for vertical splits
-      else
-        return 20                              -- default for floating (or any other)
-      end
-    end,
-    open_mapping = [[<c-\>]],
-    hide_numbers = true,
-    shade_terminals = true,
-    shading_factor = 2,
-    start_in_insert = true,
-    insert_mappings = true,
-    persist_size = true,
-    direction = "horizontal", -- set default as horizontal split
-    close_on_exit = true,
-    shell = vim.o.shell,
-    float_opts = {
-      border = "curved",
-    },
-  }
+	toggleterm.setup({
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 10 -- height for horizontal split terminals
+			elseif term.direction == "vertical" then
+				return math.floor(vim.o.columns * 0.4) -- width for vertical splits
+			else
+				return 20 -- default for floating (or any other)
+			end
+		end,
+		open_mapping = [[<c-\>]],
+		hide_numbers = true,
+		shade_terminals = true,
+		shading_factor = 2,
+		start_in_insert = true,
+		insert_mappings = true,
+		persist_size = true,
+		direction = "float", -- set default as horizontal split
+		close_on_exit = true,
+		shell = vim.o.shell,
+		float_opts = {
+			border = "curved",
+		},
+	})
 
-  function _G.set_terminal_keymaps()
-    local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-  end
+	function _G.set_terminal_keymaps()
+		local opts = { noremap = true }
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+	end
 
-  vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
-
-  -- Keybindings to override the default orientation
-  vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Toggle horizontal terminal" })
-  vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Toggle vertical terminal" })
-  vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
+	vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 end
 
 return M
